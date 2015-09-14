59acb3c0eb99741c91b741a6099afaf3ff16bfbd
frittie <frida.svensson@telia.com>
2015-09-14 11:44:49 +0200

    added items to shop

--- index.php
+++ index.php
@@ -1,5 +1,76 @@
 <?php
 require 'header.php';
+?>
 
+<div id="shop">
+	<div id="items">
+		<ul>
+			<li class="item">
+				<img src="img/darin/dd1.jpg">
+				<div class="item-info">
+					<h2>DD1</h2>
+					<p>T-shirt</p>
+					<p>249:-</p>
+				</div>
+			</li>
+
+			<li class="item">
+				<img src="img/darin/dd2.jpg">
+				<div class="item-info">
+					<h2>DD2</h2>
+					<p>T-shirt</p>
+					<p>249:-</p>
+				</div>
+			</li>
+
+			<li class="item">
+				<img src="img/darin/dd3.jpg">
+				<div class="item-info">
+					<h2>DD3</h2>
+					<p>T-shirt</p>
+					<p>249:-</p>
+				</div>
+			</li>
+
+			<li class="item">
+				<img src="img/darin/dd4.jpg">
+				<div class="item-info">
+					<h2>DD4</h2>
+					<p>Väska</p>
+					<p>99:-</p>
+				</div>
+			</li>
+
+			<li class="item">
+				<img src="img/darin/darin-maiden.jpg">
+				<div class="item-info">
+					<h2>Darin Maiden</h2>
+					<p>T-shirt</p>
+					<p>199:-</p>
+				</div>
+			</li>
+
+			<li class="item">
+				<img src="img/darin/exit.jpg">
+				<div class="item-info">
+					<h2>Exit</h2>
+					<p>Linne</p>
+					<p>199:-</p>
+				</div>
+			</li>
+
+			<li class="item">
+				<img src="img/darin/cd.jpg">
+				<div class="item-info">
+					<h2>Exit</h2>
+					<p>CD</p>
+					<p>129:-</p>
+				</div>
+			</li>			
+		</ul>
+	</div>
+</div>
+
+<?php
 require 'footer.php';
 ?>
