d86fd51eabe7914e98a21a00e5ebddcab4decefe
frittie <frida.svensson@telia.com>
2015-09-14 13:11:40 +0200

    köpvillkor

---
+++ villkor.php
@@ -0,0 +1,72 @@
+<?php
+require 'header.php';
+?>
+
+<div class="wrapper">
+	<div class="content">
+		<h1>Köpvillkor</h1>
+
+		<h2>Introduktion</h2>
+		<p>Välkommen till Merchworld AB / Merchworld.se. Följande allmänna villkor gäller användandet samt handel via vår nätbutik. Dessa villkor klargör vad Ni kan förvänta Er av oss, samt vad vi förväntar oss av Er. Våra allmäna villkor kan ändras under tidens lopp. Senaste uppdatering 2010-08-24.</p>
+		<p>Merchworld inriktar sig på merchandise för musik- och underhållningsindustrin. Nätbutiken Merchworld.se är en av våra tjänster utöver design, produktion och distribution.</p>
+
+		<h2>§1 Allmänt</h2>
+		<p>Våra produkter kan Du handla via vår hemsida. Vill Du veta hur Du går tillväga för att genomföra ett köp, klicka här för hjälp. Har Du andra frågor, klicka här för kundtjänst.</p>
+		<p>Alla varor förblir vår egendom till dess att full betalning erlagts.</p>
+		<p>Alla försök till och/eller bedrägerier polisanmäls. Merchworld förbehåller oss rätten att vid misstanke om bedrägeri häva ett köp och/eller ytterligare kontrollera köpet, även om detta innebär att leveranstiden förlängs.</p>
+		<p>Inga andra betalsätt godkänns utöver de vilka visas i kassan på hemsidan.</p>
+		<p>Merchworld reserverar sig mot mindre avvikelser gällande hur färger, material, vikt, storlekar samt tillverkning återges på hemsidan. Detta gäller även eventuella stavfel, sortiment- eller prisförändringar.</p>
+		<p>I samtliga priser till privatpersoner ingår moms med 25%.</p>
+		<p>Du måste vara över 18 år eller ha målsmans tillstånd för att kunna beställa hos oss. Lämna namn och telefonnummer till den målsman vilken godkänt köpet i meddelanderutan i kassan.</p>
+
+		<h2>§2 leverans</h2>
+		<p>Alla våra försändelser skickas med Posten Sverige AB. Du väljer leveranssätt när Du lägger din order. Din order kommer levereras till den adress Du angivit i din personliga profil. Vänligen kontrollera att adressen stämmer när du väl får din orderbekräftelse.</p>
+		<p>Övriga ordrar skickas med vanlig brevpost direkt till din brevlåda.</p>
+		<p>Beräknad leveranstid är 5-10 arbetsdagar, om inget annat anges i anslutning till varan på hemsidan.</p>
+		<p>Samtliga varor vilka visas på sidan finns i lager.</p>
+		<p>Ej uthämtad vara debiteras med 150 SEK. Skulle Du ångra dig efter att vi skickat paketet till Dig måste Du ändå hämta ut ditt paket och därefter returnera det till oss för att inte bli debiterad. Outlösta paket omfattas normalt inte av ångerrätt enligt Distansavtalslagen.</p>
+		<p>Vid transportskador skall Du anmäla detta direkt vid utlämningsstället. Upptäcker Du först skadan när Du öppnat paketet kontakta Kundtjänst. När Du återsänder varor till oss står Du själv för transportrisken.</p>
+		<p>Våra fraktkostander är följande:</p>
+		<ul>
+			<li>Sverige (0-1 kg) 39 SEK (Postförskott + 60 SEK)</li>
+			<li>Europa (0-1 kg) 69 SEK</li>
+			<li>Övriga världen (0-1 kg) 99 SEK</li>
+		</ul>
+
+		<h2>§3 betalning</h2>		
+		<p>Du har möjlighet att välja följande betalningssätt när Du lägger din order:</p>
+		<ul>
+			<li>Betalkort (Mastercard/Visa)</li>
+			<li>Klarna Faktura</li>
+			<li>Paypal</li>
+		</ul> 
+
+		<p>Vid kortbetalning betalar Du direkt i kassan i nätbutiken med ditt Visa eller Mastercard.Endast varor med ett totalt värde av 1000 sek erhåller ett kollinummer vid förfrågan. Här får Du en avi och hämtar ut ditt paket på angivet utlämningsställe. Alla andra ordrar skickas med vanlig brevpost.</p>
+		<p>Alla uppgifter Du uppger om ditt kort krypteras och är säkrade med en specifik metod genom Auriga AB. Merchworld förbehåller sig rätten att vid misstanke om obehörigt köp kontrollera lämnade uppgifter, be om ytterliggare information om kortet eller att bekräfta ordern separat genom att skicka en anvisning till faktureringsadressen innan varan levereras.</p>
+
+		<h2>§4 reklamation och returer</h2>
+		<p>I enlighet med svensk lagstiftning har Du reklamationsrätt och ångerrätt av varan om denna inte är enligt beskrivning eller har annat fel. Mer information om gällande Konsumentköplagen hittar Du här på Konsumentverkets hemsida.</p>
+		<p>Du har full ångerrätt 14 dagar från det att Du har mottagit varan. Om Du ångrar dig skall Du meddela Merchworld detta inom den angivna tiden vilken omfattas av ångerfristen. Varan får inte vara använd och skall returneras i oskadat skick. Ångerrätten gäller inte ljud- eller bildupptagningar om förseglingen brutits, eller underkläder. Öppet köp gäller inte REA-varor utan endast bytesrätt. Återbetalning kan ta upp till 30 dagar.</p>
+		<p>Vid retur eller byte står kunden för fraktkostnaderna. Vid reklamation av defekt eller felaktig vara skall detta meddelas oss snarast efter Du erhållit varan. I detta fall står Merchworld för samtliga fraktkostnader. Vänligen kontakta vår Kundtjänst, klicka här, innan Du skickar tillbaka en vara. Tänk på att Du ansvarar för varan till den når oss, det vill säga även under transporten.</p>
+
+		<h2>Såhär gör du för att returnera en vara:</h2>
+		<p>Ladda ned retursedel här. Fyll i och skriv ut.</p>
+		<p>Maila sedan returns@merchworld.se och meddela vad felet är eller varför du vill returnera varan och bifoga kvittot.</p>
+		<p>Skicka tillbaka varan, kvittot samt retursedeln till följande adress:</p>
+		<p>Merchworld AB</p>
+		<p>Tenggrenstorpsvägen 15A</p>
+		<p>462 56 VÄNERSBORG</p>
+		<p>Sweden</p> 
+
+		<h2>§5 säkerhet</h2>
+		<p>All information vid kortköp överförs krypterad via SSL-teknik, vilket gör informationen insynsskyddad och oåtkomlig för utomstående. Merchworld samarbetar med Auriga AB:s betalväxel (PSP) samt Swedbanks Babs AB för inlösen av kortbetalningar.</p>
+		<p>Enligt lag (PUL) måste vi be Dig att samtycka till att vi behandlar uppgifter om Dig. Den information vi erhåller om Dig används för att Merchworld skall kunna fullgöra våra åtaganden gentemot Dig , skicka erbjudanden eller i övriga marknadsföringssyften. Merchworld lämnar inte ut dina uppgifter till tredje part om Du inte samtycker. Du kan alltid ändra eller ta bort de uppgifter vilka finns registrerade hos oss.</p>
+
+		<h2>§6 force majeure</h2>
+		<p>Vid händelse av krig, krigsliknande händelse, inbördeskrig, revolution (viva), uppror, myndighets åtgärd, strejk, lockout, blockad, naturkatastrof eller liknande händelse, kommer Merchworld att kunna tillämpa sig av force majeure. Detta betyder att detta avtal inte behöver fullgöras till viss eller hel del. Om detta inträffar kommer vi att försöka underrätta kunden om detta.</p>
+	</div>
+</div>
+
+<?php
+require 'footer.php';
+?>
