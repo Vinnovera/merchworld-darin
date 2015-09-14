250513ab9895b225dc7b3d810ef598bb40b99b0c
frittie <frida.svensson@telia.com>
2015-09-14 14:54:46 +0200

    index for all artists

---
+++ index_eric.php
@@ -0,0 +1,78 @@
+<?php
+require 'header.php';
+
+?>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<div id="shop">
+	<div id="items">
+		<ul>
+			<li class="item">
+				<img src="img/eric_saade/sting-tygvaska.jpg">
+				<div class="item-info">
+					<h2>Sting tygväska</h2>
+					<p>Väska</p>
+					<p>149:-</p>
+				</div>
+			</li>
+
+			<li class="item">
+				<img src="img/eric_saade/aa.jpg">
+				<div class="item-info">
+					<h2>AA</h2>
+					<p>T-shirt</p>
+					<p>249:-</p>
+				</div>
+			</li>
+
+			<li class="item">
+				<img src="img/eric_saade/aa-kids.jpg">
+				<div class="item-info">
+					<h2>AA kids</h2>
+					<p>T-shirt</p>
+					<p>249:-</p>
+				</div>
+			</li>
+
+			<li class="item">
+				<img src="img/eric_saade/aa-guld.jpg">
+				<div class="item-info">
+					<h2>AA Guld</h2>
+					<p>T-shirt</p>
+					<p>249:-</p>
+				</div>
+			</li>
+
+			<li class="item">
+				<img src="img/eric_saade/aa-guld-kids.jpg">
+				<div class="item-info">
+					<h2>AA Guld kids</h2>
+					<p>T-shirt</p>
+					<p>249:-</p>
+				</div>
+			</li>
+
+			<li class="item">
+				<img src="img/eric_saade/sting.jpg">
+				<div class="item-info">
+					<h2>Sting</h2>
+					<p>T-shirt</p>
+					<p>249:-</p>
+				</div>
+			</li>
+
+			<li class="item">
+				<img src="img/eric_saade/sting-kids.jpg">
+				<div class="item-info">
+					<h2>Sting Kids</h2>
+					<p>T-shirt</p>
+					<p>249:-</p>
+				</div>
+			</li>
+			<div class="clearfix"></div>			
+		</ul>
+	</div>
+</div>
+
+<?php
+require 'footer.php';
+?>
