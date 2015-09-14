57ea57789cd17b30cd7f4c4db796a1aee9f323b6
frittie <frida.svensson@telia.com>
2015-09-14 11:15:23 +0200

    footer & img
    Footer
    Save img for darin webshop

---
+++ footer.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+</head>
+<body>
+
+	<div id="footer">
+		<div id="contact">
+			<h3>Frågor och kontakt</h3>
+			<p>Köpvillkor</p>
+			<p>Vanliga frågor</p>
+			<p>Kontakt</p>
+		</div>
+
+		<div id="about">
+			<h3>Om oss</h3>
+			<p>Om Merchworld</p>
+			<p>Tryck din egen t-shirt</p>
+			<p>Artister och företag</p>
+		</div>
+		<div class="clearfix"></div>
+		<img src="img/pay.png" alt="payment icons">
+	</div>
+
+	<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.16.0/TweenMax.min.js"></script>
+	<script src="js/main.js"></script>	
+</body>
+</html>
+
+
+
