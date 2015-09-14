5ff950a40e3dae704e6204cd3594524148d72595
Marcus Johansson <arcus.johansson@gmail.com>
2015-09-14 11:25:26 +0200

    ladda css header plus requires i index

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Artist - Merchworld</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<body>
