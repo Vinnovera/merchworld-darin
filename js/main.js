a0771e36829d0630c24e87fae2160e4d62949eb7
frittie <frida.svensson@telia.com>
2015-09-15 10:44:28 +0200

    Revert "ajax"
    This reverts commit 7a8477cbabeae1f9be45acd0e0adf7b894510467.
    
    Conflicts:
    	.sass-cache/78836178dec4ad606a3bb1e6d8e5463998a5b15e/_base.scssc
    	.sass-cache/78836178dec4ad606a3bb1e6d8e5463998a5b15e/_header.scssc
    	css/style.css

--- js/main.js
+++ js/main.js
@@ -1,29 +1,28 @@
 $(document).ready(function() {
-	
+	console.log("hello");
 	var stickyNavTop = $('.sticky-menu').offset().top;
  
 	var stickyNav = function(){
-		var scrollTop = $(window).scrollTop();
-	      
-		if (scrollTop > stickyNavTop) { 
-	    	$('.sticky-menu').addClass('sticky');
-	    	$('.navigation-menu').addClass('navigation-menu-sticky');
-	    	$('.sticky').removeClass('sticky-menu');
-		} else {
-	    	$('.sticky-menu').removeClass('sticky');
-	    	$('.navigation-menu').removeClass('navigation-menu-sticky');
-	    	$('.sticky').addClass('sticky-menu'); 
-		}
-	};
+	var scrollTop = $(window).scrollTop();
+      
+	if (scrollTop > stickyNavTop) { 
+    	$('.sticky-menu').addClass('sticky');
+    	$('.navigation-menu').addClass('navigation-menu-sticky');
+    	$('.sticky').removeClass('sticky-menu');
+	} else {
+    	$('.sticky-menu').removeClass('sticky');
+    	$('.navigation-menu').removeClass('navigation-menu-sticky');
+    	$('.sticky').addClass('sticky-menu'); 
+	}
 
-	//När man klickar på en vara ('li') gömmer man shoppen och visar en div med information om varan som man har klickat på
+	//När man klickar på en vara gömmer man listan och visar en div med information om varan som man har klickat på
 	$('.items-list li').click(function() {
 		$('.items-list').hide();
 		$('.item-show').show();
 		loadContent();
+    	return false;
 	});
 
-	//Laddar in innehållet med hjälp av ajax
 	function loadContent(){
 		$.ajax({
 			url: "items/dd1.php"
@@ -31,17 +30,11 @@ $(document).ready(function() {
 			$(".item-show").html(data);
 		});
 	}
-
-	//När man klickar på x töms 'item-show' och shoppen visas igen
-	$('item-show .fa').click(function() {
-		console.log('hej');
-		$('.item-show').empty();
-		$('.items-list').show();
-	});	
+};
  
 	stickyNav();
  
-	$(window).scroll(function() {
-	    stickyNav();
+$(window).scroll(function() {
+    stickyNav();
 	});
 });
