7134d5d33e31f77384837ad42aa3864fc121339e
Marcus Johansson <arcus.johansson@gmail.com>
2015-09-15 10:01:50 +0200

    header + footer + index
    Laddar css från header.php nu
    
    tog bort raden i footer som laddar tweenmax

--- footer.php
+++ footer.php
@@ -21,7 +21,6 @@
 	</div>
 
 	<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-	<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.16.0/TweenMax.min.js"></script>
 	<script src="js/main.js"></script>	
 </body>
 </html>
