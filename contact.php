e740946cd2f8811c5a5381de9d583c66f82caa35
frittie <frida.svensson@telia.com>
2015-09-16 15:40:18 +0200

    contact.php
    Created contact-form

--- contact.php
+++ contact.php
@@ -10,28 +10,44 @@ require 'header.php';
 		<p>Telefon: 0521 – 48 00 20</p>
 		<p>Telefontid: 9:00-11:30 , 12:30-15:30</p>
 
-		<h3>Företagsadress</h3>
+		<h2>Företagsadress</h2>
 		<p>Vi finns i Vänersborg, Sverige.</p>
+		<p>Merchworld AB</p>
+		<p>Tenggrenstorpsvägen 15 A</p>
+		<p>462 56 Vänersborg</p>
+		<p>Org nr 556679-8699</p>
 
+		<h2>Kontaktpersoner</h2>
+		<h4>Anna Foldemark Lanhage - Försäljning, Marknadsföring & Ekonomi</h4>
+		<p>E-mail: <a href="mailto:anna@merchworld.se">anna@merchworld.se</a></p>
 
+		<h4>Har du frågor om tryck?</h4>
+		<p>E-mail: <a href="mailto:print@merchworld.se">print@merchworld.se</a></p>
 
+		<h2>Kontaktformulär</h2>
+		<form>
+			<fieldset>
+				<select name="Smak">
+					<option value="1" >Allmänna frågor</option>
+				  	<option value="2">Frågor om beställningar</option>
+				 	<option value="3">Returer & reklamering</option>
+				  	<option value="4">Förfrågan / offert om tryckjobb</option>
+				 	<option value="5">Kontakta webmaster (rapportera buggar etc)</option>
+				</select><br />
 
-Merchworld AB
-Tenggrenstorpsvägen 15 A
-462 56 Vänersborg
+				<label for="name">Namn: </label>
+				<input id="name" type="text" name="Name">
 
-Org nr 556679-8699
+				<label for="mail">E-post: </label>
+				<input id="mail" type="text"  name="Mail">
 
+				<label for="subject">Ärende: </label>
+				<input id="subject" type="text"  name="Subject">
 
-KONTAKTPERSONER
-Anna Foldemark Lanhage - Försäljning, Marknadsföring & Ekonomi
-E-mail: anna@merchworld.se
-
-Har du frågor om tryck?
-E-mail: print@merchworld.se
-
-KONTAKTFORMULÄR
-		
+				<label for="textarea">Beskrivning: </label>
+				<textarea id="textarea" name="text"></textarea>
+			</fieldset>
+		</form>
 	</div>
 </div>
 
