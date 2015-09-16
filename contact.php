3f060ab39c60d569b190e16cd79749c9b9a34a37
frittie <frida.svensson@telia.com>
2015-09-16 13:49:26 +0200

    contact.php

---
+++ contact.php
@@ -0,0 +1,40 @@
+<?php
+require 'header.php';
+?>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<div class="wrapper">
+	<div class="content">
+		<h1>Kontakt</h1>
+		<p>E-mail: <a href="mailto:info@merchworld.se">info@merchworld.se</a></p>
+		<p>Telefon: 0521 – 48 00 20</p>
+		<p>Telefontid: 9:00-11:30 , 12:30-15:30</p>
+
+		<h3>Företagsadress</h3>
+		<p>Vi finns i Vänersborg, Sverige.</p>
+
+
+
+
+Merchworld AB
+Tenggrenstorpsvägen 15 A
+462 56 Vänersborg
+
+Org nr 556679-8699
+
+
+KONTAKTPERSONER
+Anna Foldemark Lanhage - Försäljning, Marknadsföring & Ekonomi
+E-mail: anna@merchworld.se
+
+Har du frågor om tryck?
+E-mail: print@merchworld.se
+
+KONTAKTFORMULÄR
+		
+	</div>
+</div>
+
+<?php
+require 'footer.php';
+?>
