54085e5000d76af8dd71cc42d527774caef8f48f
Marcus Johansson <arcus.johansson@gmail.com>
2015-09-18 15:33:35 +0200

    Updated js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,13 @@
 $(document).ready(function() {
-	
 	// cache the element
 	var $navBar = $('.menu');
 
 	// find original navigation bar position
 	var navPos = $navBar.offset().top;
-
+	
 	// on scroll
 	$(window).scroll(function() {
-
-    	// get scroll position from top of the page
-    	var scrollPos = $(this).scrollTop();
-
-		// check if scroll position is >= the nav position
-		if (scrollPos >= navPos) {
-		    $navBar.addClass('fixed');
-		} else {
-		    $navBar.removeClass('fixed');
-		}
-
+		fixedMenu();
 	});
 
 	//När man klickar på en vara ('li') gömmer man shoppen och visar en div med information om varan som man har klickat på
@@ -41,4 +30,20 @@ $(document).ready(function() {
 	$('button').click(function() {
 		$('.main-menu').slideToggle("fast");
 	});
+
+	//Set menu class to fixed on scrollevent
+	function fixedMenu() {
+		// get scroll position from top of the page
+		var scrollPos = $(this).scrollTop();
+
+		// check if scroll position is >= the nav position
+		if (scrollPos >= navPos) {
+		    $navBar.addClass('fixed');
+		} else {
+		    $navBar.removeClass('fixed');
+		}
+	}
 });
+
+
+
