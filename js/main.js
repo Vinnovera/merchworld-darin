19b405acbeda2f41492ce267f68aeb7ba7886251
Marcus Johansson <arcus.johansson@gmail.com>
2015-09-23 10:50:42 +0200

    Nuså fungerar slidetoggle mobil/tablet menyn

--- js/main.js
+++ js/main.js
@@ -1,32 +1,9 @@
 $(document).ready(function() {
-	// cache the element
-	var navBar = $('.menu');
-
-	// find original navigation bar position
-	var navPos = navBar.offset().top;
-	
-	// on scroll
-	$(window).scroll(function() {
-		fixedMenu();
-	});
 
 	//Slide toggle mobile menu
 	$('.menu button').click(function() {
-		$('.main-menu').slideToggle("fast");
+		$('.main-menu > ul').slideToggle("slow");
 	});
-
-	//Set menu class to fixed on scrollevent
-	function fixedMenu() {
-		// get scroll position from top of the page
-		var scrollPos = $(this).scrollTop();
-
-		// check if scroll position is >= the nav position
-		if (scrollPos >= navPos) {
-		    navBar.addClass('fixed');
-		} else {
-		    navBar.removeClass('fixed');
-		}
-	}
 
 	$(".fancybox").fancybox();
 });
